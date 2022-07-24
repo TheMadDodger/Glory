d1b7dc6b5647e02164bb1f5b31218646843eae3b
SPARTAN2 <>
2022-07-24 21:06:22 +0200

    Some cleanup

--- GloryEngine/GloryCore/Glory.h
+++ GloryEngine/GloryCore/Glory.h
@@ -7,11 +7,3 @@
 // USE LIB
 #define GLORY_API __declspec(dllimport)
 #endif
-
-#ifdef GLORY_CORE_EXPORTS
-// BUILD LIB
-#define GLORY_CORE_API __declspec(dllexport)
-#else
-// USE LIB
-#define GLORY_CORE_API __declspec(dllimport)
-#endif
