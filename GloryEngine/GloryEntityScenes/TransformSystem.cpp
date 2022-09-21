1924f37a620da79f25aae182e93f22b3385b5828
SPARTAN2 <unknown>
2022-09-21 22:05:28 +0200

    Fixed inverted rotation Inverted matrix aqcuired from rotation quaternion of object transform

--- GloryEngine/GloryEntityScenes/TransformSystem.cpp
+++ GloryEngine/GloryEntityScenes/TransformSystem.cpp
@@ -23,7 +23,7 @@ namespace Glory
         }
 
         glm::mat4 scale = glm::scale(glm::identity<glm::mat4>(), pComponent.Scale);
-        glm::mat4 rotation = glm::mat4_cast(pComponent.Rotation);
+        glm::mat4 rotation = glm::inverse(glm::mat4_cast(pComponent.Rotation));
         //glm::mat4 rotation = glm::eulerAngleXYX(90.0f, 90.0f, 0.0f);
         glm::mat4 translation = glm::translate(glm::identity<glm::mat4>(), pComponent.Position);
         pComponent.MatTransform = startTransform * translation * rotation * scale;
