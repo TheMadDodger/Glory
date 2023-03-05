771f0c9705a55d921ca6567b8c2c2fa0f846e6ef
Robin <robin.cordemans302@gmail.com>
2023-03-05 19:43:15 +0100

    Test out Jolt

--- GloryEngine/Engine/Modules/GloryJoltPhysics/JoltPhysicsModule.h
+++ GloryEngine/Engine/Modules/GloryJoltPhysics/JoltPhysicsModule.h
@@ -2,6 +2,17 @@
 #include <PhysicsModule.h>
 #include <Jolt/Jolt.h>
 
+#include <Jolt/RegisterTypes.h>
+#include <Jolt/Core/Factory.h>
+#include <Jolt/Core/TempAllocator.h>
+#include <Jolt/Core/JobSystemThreadPool.h>
+#include <Jolt/Physics/PhysicsSettings.h>
+#include <Jolt/Physics/PhysicsSystem.h>
+#include <Jolt/Physics/Collision/Shape/BoxShape.h>
+#include <Jolt/Physics/Collision/Shape/SphereShape.h>
+#include <Jolt/Physics/Body/BodyCreationSettings.h>
+#include <Jolt/Physics/Body/BodyActivationListener.h>
+
 namespace Glory
 {
     class JoltPhysicsModule : public PhysicsModule
