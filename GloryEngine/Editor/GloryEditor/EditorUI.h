3343b203458ec045d3a16989abd2d762ac857af1
Robin <robin.cordemans302@gmail.com>
2023-03-14 00:40:13 +0100

    Add module settings for Jolt

--- GloryEngine/Editor/GloryEditor/EditorUI.h
+++ GloryEngine/Editor/GloryEditor/EditorUI.h
@@ -41,6 +41,8 @@ namespace Glory::Editor
 		static GLORY_EDITOR_API bool InputInt3(YAMLFileRef& file, const std::filesystem::path& path, const int min = INT_MIN, const int max = INT_MAX, const int steps = 1);
 		static GLORY_EDITOR_API bool InputInt4(std::string_view label, glm::ivec4* value, const int min = INT_MIN, const int max = INT_MAX, const int steps = 1);
 		static GLORY_EDITOR_API bool InputInt4(YAMLFileRef& file, const std::filesystem::path& path, const int min = INT_MIN, const int max = INT_MAX, const int steps = 1);
+		
+		static GLORY_EDITOR_API bool InputUInt(std::string_view label, uint32_t* value, const uint32_t min = 0, const uint32_t max = INT_MAX, const uint32_t steps = 1);
 
 		static GLORY_EDITOR_API bool InputDouble(std::string_view label, double* value, const double slowSteps = 0.01f, const double fastSteps = 0.1f);
 		static GLORY_EDITOR_API bool InputDouble(YAMLFileRef& file, const std::filesystem::path& path, const double slowSteps = 0.01f, const double fastSteps = 0.1f);
