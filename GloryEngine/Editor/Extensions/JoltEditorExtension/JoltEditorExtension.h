5d9e4e476189ff0c3d2e0dfe7ebab15039828525
Robin <robin.cordemans302@gmail.com>
2023-03-25 21:46:17 +0100

    Add editor extension for jolt

---
+++ GloryEngine/Editor/Extensions/JoltEditorExtension/JoltEditorExtension.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <BaseEditorExtension.h>
+#include <IPlayModeHandler.h>
+
+extern "C" GLORY_EDITOR_EXTENSION_API Glory::Editor::BaseEditorExtension* LoadExtension();
+extern "C" GLORY_EDITOR_EXTENSION_API void SetContext(Glory::GloryContext * pContext, ImGuiContext * pImGUIContext);
+
+namespace Glory::Editor
+{
+    class JoltEditorExtension : public BaseEditorExtension, IPlayModeHandler
+    {
+    public:
+        JoltEditorExtension();
+        virtual ~JoltEditorExtension();
+
+    private:
+        virtual void RegisterEditors() override;
+
+        virtual const char* ModuleName() override;
+        virtual void HandleStart(Module* pModule) override;
+        virtual void HandleStop(Module* pModule) override;
+        virtual void HandleUpdate(Module* pModule) override;
+    };
+}
---
+++ GloryEngine/Editor/Extensions/JoltEditorExtension/premake5.lua
@@ -0,0 +1,104 @@
+project "JoltEditorExtension"
+	kind "SharedLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "Off"
+
+	targetdir ("%{modulesOutDir}/GloryJoltPhysics/Editor/Extension")
+	objdir ("%{outputDir}")
+
+	files
+	{
+		"**.h",
+		"**.cpp",
+		"premake5.lua"
+	}
+
+	vpaths
+	{
+	}
+
+	includedirs
+	{
+		"%{DepsIncludeDir}",
+
+		"%{IncludeDir.glm}",
+
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.yaml_cpp}",
+		"%{GloryIncludeDir.core}",
+		"%{GloryIncludeDir.editor}",
+		"%{GloryIncludeDir.ImGui}",
+		"%{GloryIncludeDir.jolt}",
+
+		"%{IncludeDir.FA}",
+
+		"%{IncludeDir.ECS}",
+		"%{IncludeDir.Reflect}",
+
+		"%{rootDir}/submodules/JoltPhysics",
+	}
+
+	libdirs
+	{
+		"%{DepsLibDir}",
+
+		"%{LibDirs.ImGui}",
+		"%{LibDirs.implot}",
+		"%{LibDirs.glory}",
+		"%{modulesDir}/GloryJoltPhysics",
+		"%{LibDirs.yaml_cpp}",
+
+		"%{LibDirs.GloryECS}",
+	}
+
+	links
+	{
+		"GloryCore",
+		"GloryEditor",
+		"GloryJoltPhysics",
+		"shaderc",
+		"shaderc_combined",
+		"shaderc_shared",
+		"ImGui",
+		"ImGuizmo",
+		"implot",
+		"yaml-cpp",
+
+		"GloryECSStatic",
+		"GloryReflectStatic"
+	}
+
+	defines
+	{
+		"GLORY_EXPORTS",
+		"GLORY_EDITOR_EXPORTS",
+		"GLORY_EDITOR_EXTENSION_EXPORTS",
+		"GLORY_UUID_DEFINED"
+	}
+
+	filter "system:windows"
+		systemversion "10.0.19041.0"
+		toolset "v143"
+
+		defines
+		{
+			"_LIB"
+		}
+
+	filter "platforms:Win32"
+		architecture "x86"
+		defines "WIN32"
+
+	filter "platforms:x64"
+		architecture "x64"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		defines "_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		runtime "Release"
+		defines "NDEBUG"
+		optimize "On"
