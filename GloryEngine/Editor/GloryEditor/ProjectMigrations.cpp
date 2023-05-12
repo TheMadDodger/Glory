74520950b22271a27fbeb817ce138f9041c9a029
Robin <unknown>
2023-05-12 02:47:16 +0200

    Migrate image datas in materials to default texture of the image

--- GloryEngine/Editor/GloryEditor/ProjectMigrations.cpp
+++ GloryEngine/Editor/GloryEditor/ProjectMigrations.cpp
@@ -3,6 +3,8 @@
 
 #include <JSONRef.h>
 #include <TextureData.h>
+#include <MaterialInstanceData.h>
+#include <NodeRef.h>
 
 namespace Glory::Editor
 {
@@ -21,27 +23,49 @@ namespace Glory::Editor
         /* Run migrations */
         if (Version::Compare(version, { 0,1,1,0 }, true) < 0)
         {
-            Migrate_0_1_1_AddDefaultTextureInImageDatas(pProject);
+            Migrate_0_1_1_DefaultTextureAndMaterialProperties(pProject);
         }
 
         /* Update version to current */
         editorVersion.SetString(GloryEditorVersion);
     }
 
-    void Migrate_0_1_1_AddDefaultTextureInImageDatas(ProjectSpace* pProject)
+    void Migrate_0_1_1_DefaultTextureAndMaterialProperties(ProjectSpace* pProject)
     {
+        Debug::LogInfo("0.1.1> Migrating image datas to include default texture");
+
         JSONFileRef& projectFile = pProject->ProjectFile();
         JSONValueRef assets = projectFile["Assets"];
 
         std::vector<std::string_view> imageAssets;
+        std::vector<std::string_view> materialAssets;
+        std::vector<std::string_view> materialInstanceAssets;
+
+        const uint32_t imageDataHash = ResourceType::GetHash<ImageData>();
+        const uint32_t textureDataHash = ResourceType::GetHash<TextureData>();
+        const uint32_t materialDataHash = ResourceType::GetHash<MaterialData>();
+        const uint32_t materialInstanceDataHash = ResourceType::GetHash<MaterialInstanceData>();
 
         for (rapidjson::Value::ConstMemberIterator itor = assets.begin(); itor != assets.end(); ++itor)
         {
             JSONValueRef asset = assets[itor->name.GetString()];
             const uint32_t hash = asset["Metadata/Hash"].AsUInt();
-            if (hash != ResourceType::GetHash<ImageData>()) continue;
-            imageAssets.push_back(itor->name.GetString());
+
+            if (hash == imageDataHash)
+            {
+                imageAssets.push_back(itor->name.GetString());
+            }
+            else if (hash == materialDataHash)
+            {
+                materialAssets.push_back(itor->name.GetString());
+            }
+            else if (hash == materialInstanceDataHash)
+            {
+                materialInstanceAssets.push_back(itor->name.GetString());
+            }
         }
+
+        std::map<std::string_view, UUID> imageIDToTextureID;
 
         for (size_t i = 0; i < imageAssets.size(); i++)
         {
@@ -49,8 +73,9 @@ namespace Glory::Editor
             JSONValueRef asset = assets[assetID];
 
             UUID newUUID{};
-            std::to_string(newUUID);
-            JSONValueRef defaultTextureAsset = assets[std::to_string(newUUID)];
+            const std::string uuidString = std::to_string(newUUID);
+            imageIDToTextureID.emplace(assetID, newUUID);
+            JSONValueRef defaultTextureAsset = assets[uuidString];
             JSONValueRef location = defaultTextureAsset["Location"];
             JSONValueRef metaData = defaultTextureAsset["Metadata"];
             location["Path"].SetStringFromView(asset["Location/Path"].AsString());
@@ -64,6 +89,81 @@ namespace Glory::Editor
             metaData["UUID"].SetUInt64(newUUID);
             metaData["Hash"].SetUInt(ResourceType::GetHash<TextureData>());
             metaData["SerializedVersion"].SetUInt(0);
+
+            std::stringstream debug;
+            debug << "0.1.1> Migrated image data " << assetID << " to include default texture";
+            Debug::LogInfo(debug.str());
+        }
+
+        Debug::LogInfo("0.1.1> Migrating material propeties");
+        for (size_t i = 0; i < materialAssets.size(); ++i)
+        {
+            const std::string_view materialAssetID = materialAssets[i];
+            JSONValueRef asset = assets[materialAssetID];
+            JSONValueRef location = asset["Location"];
+            const std::string_view path = location["Path"].AsString();
+
+            std::filesystem::path absolutePath = pProject->RootPath();
+            absolutePath.append("Assets").append(path);
+
+            if (!std::filesystem::exists(absolutePath)) continue;
+
+            YAMLFileRef materialFile{ absolutePath };
+            materialFile.Load();
+            NodeValueRef properties = materialFile["Properties"];
+            for (size_t i = 0; i < properties.Size(); ++i)
+            {
+                NodeValueRef property = properties[i];
+                NodeValueRef propetyHash = property["TypeHash"];
+                const uint32_t typeHash = propetyHash.As<uint32_t>();
+                if (typeHash != imageDataHash) continue;
+                propetyHash.Set(textureDataHash);
+                NodeValueRef propertyValue = property["Value"];
+                const UUID uuid = propertyValue.As<uint64_t>();
+                auto itor = imageIDToTextureID.find(std::to_string(uuid));
+                if (itor == imageIDToTextureID.end()) continue;
+                const UUID newUUID = itor->second;
+                propertyValue.Set((uint64_t)newUUID);
+
+                std::stringstream debug;
+                debug << "0.1.1> Migrated material " << materialAssetID << " propery " << property["DisplayName"].As<std::string>() << " to default texture " << newUUID;
+                Debug::LogInfo(debug.str());
+            }
+            materialFile.Save();
+        }
+
+        Debug::LogInfo("0.1.1> Migrating material instance property overrides");
+        for (size_t i = 0; i < materialInstanceAssets.size(); ++i)
+        {
+            const std::string_view materialAssetID = materialInstanceAssets[i];
+            JSONValueRef asset = assets[materialAssetID];
+            JSONValueRef location = asset["Location"];
+            const std::string_view path = location["Path"].AsString();
+
+            std::filesystem::path absolutePath = pProject->RootPath();
+            absolutePath.append("Assets").append(path);
+
+            if (!std::filesystem::exists(absolutePath)) continue;
+
+            YAMLFileRef materialFile{ absolutePath };
+            materialFile.Load();
+            NodeValueRef overrides = materialFile["Overrides"];
+            for (size_t i = 0; i < overrides.Size(); ++i)
+            {
+                NodeValueRef override = overrides[i];
+                NodeValueRef overrideValue = override["Value"];
+                if (!overrideValue.IsScalar()) continue;
+                const UUID uuid = overrideValue.As<uint64_t>();
+                auto itor = imageIDToTextureID.find(std::to_string(uuid));
+                if (itor == imageIDToTextureID.end()) continue;
+                const UUID newUUID = itor->second;
+                overrideValue.Set((uint64_t)newUUID);
+
+                std::stringstream debug;
+                debug << "0.1.1> Migrated material instance " << materialAssetID << " propery " << override["DisplayName"].As<std::string>() << " to default texture " << newUUID;
+                Debug::LogInfo(debug.str());
+            }
+            materialFile.Save();
         }
     }
 }
