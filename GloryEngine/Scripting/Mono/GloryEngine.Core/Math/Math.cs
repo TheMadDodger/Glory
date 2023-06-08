b9eb4c902dca4c21b380672297dff877aef395c9
Robin <unknown>
2023-06-09 00:53:08 +0200

    More math functions, but maybe I should use System.Numerics instead

--- GloryEngine/Scripting/Mono/GloryEngine.Core/Math/Math.cs
+++ GloryEngine/Scripting/Mono/GloryEngine.Core/Math/Math.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 namespace GloryEngine
 {
@@ -7,8 +6,27 @@ namespace GloryEngine
     {
         #region Methods
 
+        /// <summary>
+        /// Generate a matrix that looks at a certain object from a point
+        /// </summary>
+        /// <param name="eye">Position of the eye</param>
+        /// <param name="center">Position to look at</param>
+        /// <param name="up">Up vector</param>
+        /// <returns></returns>
         public static Mat4 LookAt(Vector3 eye, Vector3 center, Vector3 up)
             => Math_LookAt(eye, center, up);
+
+        /// <summary>
+        /// Linear interpolate between 2 floats
+        /// </summary>
+        /// <param name="a">Starting value</param>
+        /// <param name="b">Target value</param>
+        /// <param name="t">Lerp factor</param>
+        /// <returns>The lerped value</returns>
+        public static float Lerp(float a, float b, float t)
+        {
+            return a + (b - a) * t;
+        }
 
         #endregion
 
