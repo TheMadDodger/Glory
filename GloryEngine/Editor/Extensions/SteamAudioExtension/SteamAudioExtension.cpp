3e24a6cd464734d866788da2d3488152d7fcdd9c
Robin <robin.cordemans302@gmail.com>
2024-06-24 23:13:34 +0200

    Add steam audio editor extension

---
+++ GloryEngine/Editor/Extensions/SteamAudioExtension/SteamAudioExtension.cpp
@@ -0,0 +1,55 @@
+#include "SteamAudioExtension.h"
+
+#include <Debug.h>
+#include <Engine.h>
+#include <AudioComponents.h>
+#include <SceneManager.h>
+#include <SteamAudioModule.h>
+
+#include <EntitySceneObjectEditor.h>
+#include <CreateEntityObjectsCallbacks.h>
+#include <CreateObjectAction.h>
+#include <EntityEditor.h>
+#include <MainEditor.h>
+#include <EditorApplication.h>
+
+#include <IconsFontAwesome6.h>
+
+EXTENSION_CPP(SteamAudioExtension)
+
+namespace Glory::Editor
+{
+	CREATE_OBJECT_CALLBACK_CPP(AudioSource, AudioSource, ());
+	CREATE_OBJECT_CALLBACK_CPP(AudioListener, AudioListener, ());
+
+	void SteamAudioExtension::HandleStop(Module* pModule)
+	{
+		Engine* pEngine = EditorApplication::GetInstance()->GetEngine();
+		SteamAudioModule* pAudio = pEngine->GetOptionalModule<SteamAudioModule>();
+	}
+
+	SteamAudioExtension::SteamAudioExtension()
+	{
+	}
+
+	SteamAudioExtension::~SteamAudioExtension()
+	{
+	}
+
+	void SteamAudioExtension::Initialize()
+	{
+		Engine* pEngine = EditorApplication::GetInstance()->GetEngine();
+		Reflect::SetReflectInstance(&pEngine->Reflection());
+
+		pEngine->GetSceneManager()->ComponentTypesInstance();
+
+		//Editor::RegisterEditor<AudioDataEditor>();
+		//EntitySceneObjectEditor::AddComponentIcon<AudioSource>(ICON_FA_VOLUME_HIGH);
+		//EntitySceneObjectEditor::AddComponentIcon<AudioListener>(ICON_FA_HEADPHONES);
+
+		EditorPlayer::RegisterLoopHandler(this);
+
+		//OBJECT_CREATE_MENU(AudioSource, AudioSource);
+		//OBJECT_CREATE_MENU(AudioListener, AudioListener);
+	}
+}
