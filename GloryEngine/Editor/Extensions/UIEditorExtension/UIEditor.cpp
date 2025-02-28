236ae31cc4daaedb2e19f26b33b84f29f1bac613
Robin <robin.cordemans302@gmail.com>
2025-02-28 14:54:26 +0100

    Draw UI document output texture in ui editor

--- GloryEngine/Editor/Extensions/UIEditorExtension/UIEditor.cpp
+++ GloryEngine/Editor/Extensions/UIEditorExtension/UIEditor.cpp
@@ -13,13 +13,14 @@
 #include <UIDocument.h>
 #include <MeshData.h>
 #include <NodeRef.h>
+#include <ImGuiHelpers.h>
 
 #include <fstream>
 #include <sstream>
 
 namespace Glory::Editor
 {
-	UIEditor::UIEditor() : EditorWindowTemplate("UI Editor", 600.0f, 600.0f), m_pDocument(nullptr)
+	UIEditor::UIEditor() : EditorWindowTemplate("UI Editor", 600.0f, 600.0f), m_pDocument(nullptr), m_Resolution(1920.0f, 1080.0f)
 	{
 	}
 
@@ -56,8 +57,8 @@ namespace Glory::Editor
 
 		RenderTextureCreateInfo uiTextureInfo;
 		uiTextureInfo.HasDepth = false;
-		uiTextureInfo.Width = uint32_t(1920.0f);
-		uiTextureInfo.Height = uint32_t(1080.0f);
+		uiTextureInfo.Width = m_Resolution.x;
+		uiTextureInfo.Height = m_Resolution.y;
 		uiTextureInfo.Attachments.push_back(Attachment("UIColor", PixelFormat::PF_RGBA, PixelFormat::PF_R8G8B8A8Srgb, Glory::ImageType::IT_2D, Glory::ImageAspect::IA_Color, DataType::DT_Float));
 
 		RenderTexture* pTexture = pResourceManager->CreateRenderTexture(uiTextureInfo);
@@ -66,7 +67,47 @@ namespace Glory::Editor
 
 	void UIEditor::OnGUI()
 	{
-		//if (m_EditingDocument == 0 || !m_pDocument) return;
+		if (m_EditingDocument == 0 || !m_pDocument) return;
+
+		RenderTexture* pUITexture = m_pDocument->GetUITexture();
+		if (pUITexture == nullptr) return;
+		Texture* pTexture = pUITexture->GetTextureAttachment(0);
+
+		uint32_t width, height;
+		pUITexture->GetDimensions(width, height);
+		float textureAspect = (float)width / (float)height;
+
+		ImVec2 pos = ImGui::GetWindowPos();
+		ImVec2 vMin = ImGui::GetWindowContentRegionMin();
+		pos = pos + vMin;
+		ImVec2 vMax = ImGui::GetWindowContentRegionMax();
+		vMax = vMax - vMin;
+		float maxWidth = vMax.x;
+		float maxHeight = vMax.y;
+
+		float actualHeight = maxWidth / textureAspect;
+
+		if (actualHeight > maxHeight)
+		{
+			float diff = actualHeight - maxHeight;
+			float widthOffset = diff * textureAspect;
+			maxWidth -= widthOffset;
+			actualHeight = maxWidth / textureAspect;
+		}
+
+		const ImVec2 halfMax = vMax / 2.0f;
+		const ImVec2 center = pos + halfMax;
+
+		const ImVec2 halfOffsets = ImVec2(maxWidth / 2.0f, actualHeight / 2.0f);
+
+		EditorRenderImpl* pRenderImpl = EditorApplication::GetInstance()->GetEditorPlatform().GetRenderImpl();
+
+		const ImVec2 topLeft = center - halfOffsets;
+		const ImVec2 bottomRight = center + halfOffsets;
+
+		ImGui::GetWindowDrawList()->AddImage(
+			pRenderImpl->GetTextureID(pTexture), topLeft,
+			bottomRight, ImVec2(0, 1), ImVec2(1, 0));
 	}
 
 	void UIEditor::Update()
@@ -83,7 +124,7 @@ namespace Glory::Editor
 		data.m_DocumentID = m_EditingDocument;
 		data.m_ObjectID = 0;
 		data.m_TargetCamera = 0;
-		data.m_Resolution = glm::vec2(1920.0f, 1080.0f);
+		data.m_Resolution = glm::vec2(m_Resolution.x, m_Resolution.y);
 
 		pRenderer->DrawDocument(m_pDocument, data);
 	}
